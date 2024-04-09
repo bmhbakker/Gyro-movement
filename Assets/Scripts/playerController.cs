76f60571f58db831a87b6ece269762b0bcfe26b4
Bram Bakker <bram.bakker1999@hotmail.nl>
2024-04-09 10:07:54 +0200

    Basic movement setup

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector3 currentVelocity;
+
+    public float maxSpeed = 10f;
+    public float acceleration = 0.5f;
+    public float deceleration = 5f;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate() {
+        Move();
+
+        if (Input.GetKey(KeyCode.Space) && controller.isGrounded) {
+        }
+    }
+
+    private void Move() {
+        Vector3 desiredMoveDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+        Vector3 targetVelocity = desiredMoveDirection * maxSpeed;
+
+        currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, Time.deltaTime * acceleration);
+
+        if (moveInput == Vector2.zero) {
+            currentVelocity = Vector3.Lerp(currentVelocity, Vector3.zero, Time.deltaTime * deceleration);
+        }
+
+        currentVelocity = transform.TransformDirection(currentVelocity);
+
+        controller.Move(currentVelocity * Time.deltaTime);
+
+        Debug.Log(currentVelocity);
+    }
+
+    public void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+    }
+}
